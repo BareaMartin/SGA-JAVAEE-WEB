eb81226b00d31e6f75be918dcbbd421160ccaf80
BareaMartin <martin.barea94@gmail.com>
2021-09-14 15:47:53 -0300

    Update-14-09-2021
    Updated with Facelets interactive interface, EJB transactions, context rollback calls

--- sga-jee-web/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
+++ sga-jee-web/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -2,20 +2,25 @@ package mx.com.gm.sga.servicio;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import mx.com.gm.sga.datos.PersonaDao;
 import mx.com.gm.sga.domain.Persona;
 
 @Stateless
-public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService{
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService {
 
-    @Inject 
+    @Inject
     private PersonaDao personaDao;
-    
+
+    @Resource
+    private SessionContext context;
+
     @Override
     public List<Persona> listarPersonas() {
-       return personaDao.findAllPersonas();
+        return personaDao.findAllPersonas();
     }
 
     @Override
@@ -35,12 +40,17 @@ public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService{
 
     @Override
     public void modificarPersona(Persona persona) {
-        personaDao.updatePersona(persona);
+        try {
+            personaDao.updatePersona(persona);
+        } catch (Throwable t) {
+            context.setRollbackOnly();
+            t.printStackTrace(System.out);
+        }
     }
 
     @Override
     public void eliminarPersona(Persona persona) {
         personaDao.deletePersona(persona);
     }
-    
+
 }
